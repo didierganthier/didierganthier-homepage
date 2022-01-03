ac3b645316ed695c7e87c1f412a924389dc1d821
didierganthier <didierganthierperan@gmail.com>
2022-01-03 14:37:01 -0500

    fonts added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ddd32ce6c009683"; }
+/******/ 		__webpack_require__.h = function() { return "2d50fbb14968d77b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Fonts from "../components/fonts";
 import Layout from "../components/layouts/main";
 import theme from "../libs/theme";
 
 const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
+            <Fonts/>
         <Layout router={router}>
             <Component {...pageProps} key={router.route} />
         </Layout>
