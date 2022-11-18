4fa96bd9cdb549e36dd57e910a1b33bd50a58ec6
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-18 09:40:46 -0500

    Add new blog post

--- pages/posts.js
+++ pages/posts.js
@@ -3,6 +3,7 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 import thumbSmartContract from '../public/images/contents/medium-smart-contract.jpeg'
+import didierResized from '../public/images/contents/didier-resize.jpeg'
 
 const Posts = () => (
   <Layout title="Posts">
@@ -17,6 +18,11 @@ const Posts = () => (
             title="Write a Plane Ticket Booking Smart Contract in Solidity"
             thumbnail={thumbSmartContract}
             href="https://coinsbench.com/write-a-plane-ticket-booking-smart-contract-in-solidity-334a8e03bb64"
+          />
+          <GridItem
+            title="The biggest non-technical lessons learned in my journey as a developer"
+            thumbnail={didierResized}
+            href="https://medium.com/@didierganthier/the-biggest-non-technical-lessons-learned-in-my-journey-as-a-developer-96ec948c3df7"
           />
         </SimpleGrid>
       </Section>
