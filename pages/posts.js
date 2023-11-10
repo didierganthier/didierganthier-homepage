342f61c3cf90bd702c2e2a03941b5a9182547e4e
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-11-10 18:27:05 -0500

    Add new articles

--- pages/posts.js
+++ pages/posts.js
@@ -3,7 +3,10 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 import thumbSmartContract from '../public/images/contents/medium-smart-contract.jpeg'
-import didierResized from '../public/images/contents/didier-resize.jpeg'
+import microverseImg from '../public/images/contents/microverse.png'
+import gradientImg from '../public/images/contents/gradient-container.png'
+import absurdismImg from '../public/images/contents/absurdism.jpg'
+
 
 const Posts = () => (
   <Layout title="Posts">
@@ -21,8 +24,18 @@ const Posts = () => (
           />
           <GridItem
             title="The biggest non-technical lessons learned in my journey as a developer"
-            thumbnail={didierResized}
+            thumbnail={microverseImg}
             href="https://medium.com/@didierganthier/the-biggest-non-technical-lessons-learned-in-my-journey-as-a-developer-96ec948c3df7"
+          />
+          <GridItem
+            title="Creating a Linear Gradient Container in Flutter"
+            thumbnail={gradientImg}
+            href="https://medium.com/@didierganthier/creating-a-linear-gradient-container-in-flutter-39b64f77d9da"
+          />
+          <GridItem
+            title="Embracing the Paradox: The Pursuit of Meaning in an Absurd World"
+            thumbnail={absurdismImg}
+            href="https://medium.com/@didierganthier/embracing-the-paradox-the-pursuit-of-meaning-in-an-absurd-world-8fb1512bc02e"
           />
         </SimpleGrid>
       </Section>
