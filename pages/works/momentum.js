211518ad8d196f7cdb3c224c1e822fb9bf33b30b
Didier Ganthier <didierganthierperan@gmail.com>
2025-12-10 14:07:41 -0500

    Remove unused import for ExternalLinkIcon in momentum.js

--- pages/works/momentum.js
+++ pages/works/momentum.js
@@ -1,5 +1,4 @@
 import { Container, Badge, Link, ListItem, List } from "@chakra-ui/react";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { Title, WorkImage, Meta } from "../../components/work";
 import P from "../../components/paragraph";
 import Layout from "../../components/layouts/article";
